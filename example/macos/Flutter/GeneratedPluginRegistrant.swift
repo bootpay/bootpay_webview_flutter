c3ad15462ae38142d3bd2f714416554700638d4c
ehowlsla <ehowlsla@bootpay.co.kr>
2021-07-13 13:59:55 +0900

    ios build bug fixed

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import bootpay_webview_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  BootpayWebviewFlutterPlugin.register(with: registry.registrar(forPlugin: "BootpayWebviewFlutterPlugin"))
 }
--- ios/Classes/BootpayWebviewFlutterPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface BootpayWebviewFlutterPlugin : NSObject<FlutterPlugin>
-@end
